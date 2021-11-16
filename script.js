58ff16106aef771b038816d76d141585e24e489d
ezekielswanson <swansonezekiel@gmail.com>
2021-11-16 17:00:43 -0600

    Updated html.

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 const lightbox = document.createElement('div');
 //Doing this so it's easy to select in css
 lightbox.id = "lightbox";
-document.body.appendChild(lightbox);
+document.getElementsByClassName('section-three').appendChild(lightbox);
 
 
 
-const img = document.getElementsByClassName('active');
+const img = document.getElementsByClassName('light-box');
+
 img.forEach(image => {
-    a.addEventListner('click', (e) => {
-        /*Everytime you click on image with class of big you act active class
+    img.addEventListner('click', (e) => {
+        /*Everytime you click on image with class active class
         to it to enable the lightbox styling
         */
         lightbox.classList.add('active');
