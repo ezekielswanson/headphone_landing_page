d5591127f5c743af38e485c4fe9b967592388540
ezekielswanson <swansonezekiel@gmail.com>
2021-11-22 21:23:44 -0600

    Updated js file.

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
+const texts = ["Day", "Work", "Life"];
 
-const lightbox = document.createElement('div');
-//Doing this so it's easy to select in css
-lightbox.id = "lightbox";
-document.getElementsByClassName('section-three').appendChild(lightbox);
+//Create a counter that goes through each word one by one 
+//And then loop through each index of the word
 
+let count = 0;
+let index = 0;
+//Grabbing the actual words and putting it into the var currentText 
+let currentText = '';
+let letter = '';
 
+(function type() {
+    if(count === texts.lenght) {
+        count = 0;
+    }
 
-const img = document.getElementsByClassName('light-box');
+    currentText = texts[count];
+    letter = currentText.slice(0,index++);
+    letter = document.querySelector('.typing').textContent
+        if (letter.length === currentText.length) {
+            count++;
+            index = 0;
+        }
+        
+        setTimeout(type, 400);
 
-img.forEach(image => {
-    img.addEventListner('click', (e) => {
-        /*Everytime you click on image with class active class
-        to it to enable the lightbox styling
-        */
-        lightbox.classList.add('active');
+}());
 
-
-    });
-
-
-
-});
+//Extra parenetheeses allows you to call the function immediately 
